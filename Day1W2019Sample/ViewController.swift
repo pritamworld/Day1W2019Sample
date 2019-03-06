e6fe7e0622f0fb7f2430c9cb22e478cbdc2f631c
moxDroid Labs Inc <pritam.world@gmail.com>
2019-03-05 20:11:45 -0500

    Code changes

--- Day1W2019Sample/ViewController.swift
+++ Day1W2019Sample/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtNumber2: UITextField!
+    @IBOutlet weak var txtNumber1: UITextField!
+    
+    @IBOutlet weak var lblMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        //self.lblMessage.text = "Hello World!!!"
+        lblMessage.text = "Hello World!!!"
     }
 
-
+    @IBAction func btnSubmitClick(_ sender: UIButton) {
+        lblMessage.text = "I am from Button Touch"
+    }
+    
+    @IBAction func btnOperator(_ sender: UIButton) {
+        var a = Int(txtNumber1.text!)!
+        var b = Int(txtNumber2.text!)!
+        var c = 0
+        switch(sender.tag)
+        {
+            case 0: c = a + b
+            case 1: c = a - b
+            case 2: c = a * b
+            case 3: c = a / b
+        default:
+            c = 0
+        }
+        
+        //lblMessage.text = String(c)
+        lblMessage.text = "\(c)"
+        
+    }
 }
 
